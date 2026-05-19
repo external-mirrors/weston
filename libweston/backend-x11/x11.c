external-mirrors/weston

commit 99527e6b927b261f21d2a3428f6ac65c69e48713

input: Introduce weston_key_event struct
Rather than passing a time stamp, key, key state and key event state
use a weston_key_event struct to pass by all that using it.

This would allow in further patches to attach additional information
like a flow id used by Perfetto debug annotations for input events.

This patch has no functional change as it is now.

All the callees will just will extract the required information out of
struct weston_key_event.

Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

libweston/backend-x11/x11.c
@@ -1630,11 +1630,15 @@ x11_backend_handle_event(int fd, uint32_t mask, void *data)
 				 * event below. */
 				update_xkb_state_from_core(b, key_release->state);
 				weston_compositor_get_time(&time);
-				notify_key(&b->core_seat,
-					   &time,
-					   key_release->detail - 8,
-					   WL_KEYBOARD_KEY_STATE_RELEASED,
-					   STATE_UPDATE_AUTOMATIC);
+
+				struct weston_key_event key_event;
+
+				weston_key_event_init(&key_event, &time, &b->core_seat,
+						      key_release->detail - 8,
+						      WL_KEYBOARD_KEY_STATE_RELEASED,
+						      STATE_UPDATE_AUTOMATIC);
+
+				notify_key(&key_event);
 				free(b->prev_event);
 				b->prev_event = NULL;
 				break;
@@ -1675,12 +1679,15 @@ x11_backend_handle_event(int fd, uint32_t mask, void *data)
 			if (!b->has_xkb)
 				update_xkb_state_from_core(b, key_press->state);
 			weston_compositor_get_time(&time);
-			notify_key(&b->core_seat,
-				   &time,
-				   key_press->detail - 8,
-				   WL_KEYBOARD_KEY_STATE_PRESSED,
-				   b->has_xkb ? STATE_UPDATE_NONE :
-						STATE_UPDATE_AUTOMATIC);
+
+			struct weston_key_event key_event;
+
+			weston_key_event_init(&key_event, &time, &b->core_seat,
+					      key_release->detail - 8,
+					      WL_KEYBOARD_KEY_STATE_PRESSED,
+					      b->has_xkb ? STATE_UPDATE_NONE : STATE_UPDATE_AUTOMATIC);
+			notify_key(&key_event);
+
 			break;
 		case XCB_KEY_RELEASE:
 			/* If we don't have XKB, we need to use the lame
@@ -1691,11 +1698,13 @@ x11_backend_handle_event(int fd, uint32_t mask, void *data)
 			}
 			key_release = (xcb_key_press_event_t *) event;
 			weston_compositor_get_time(&time);
-			notify_key(&b->core_seat,
-				   &time,
-				   key_release->detail - 8,
-				   WL_KEYBOARD_KEY_STATE_RELEASED,
-				   STATE_UPDATE_NONE);
+
+			struct weston_key_event w_key_event;
+
+			weston_key_event_init(&w_key_event, &time, &b->core_seat,
+					      key_release->detail - 8,
+					      WL_KEYBOARD_KEY_STATE_RELEASED, STATE_UPDATE_NONE);
+			notify_key(&w_key_event);
 			break;
 		case XCB_BUTTON_PRESS:
 		case XCB_BUTTON_RELEASE:
@@ -1824,11 +1833,14 @@ x11_backend_handle_event(int fd, uint32_t mask, void *data)
 		key_release = (xcb_key_press_event_t *) b->prev_event;
 		update_xkb_state_from_core(b, key_release->state);
 		weston_compositor_get_time(&time);
-		notify_key(&b->core_seat,
-			   &time,
-			   key_release->detail - 8,
-			   WL_KEYBOARD_KEY_STATE_RELEASED,
-			   STATE_UPDATE_AUTOMATIC);
+
+		struct weston_key_event lshift_key_event;
+
+		weston_key_event_init(&lshift_key_event, &time, &b->core_seat,
+				      key_release->detail - 8,
+				      WL_KEYBOARD_KEY_STATE_RELEASED, STATE_UPDATE_AUTOMATIC);
+		notify_key(&lshift_key_event);
+
 		free(b->prev_event);
 		b->prev_event = NULL;
 		break;

desktop-shell/shell.c
@@ -4189,11 +4189,11 @@ switcher_destroy(struct switcher *switcher)
 }
 
 static void
-switcher_key(struct weston_keyboard_grab *grab,
-	     const struct timespec *time, uint32_t key, uint32_t state_w)
+switcher_key(struct weston_keyboard_grab *grab, const struct weston_key_event *key_event)
 {
 	struct switcher *switcher = container_of(grab, struct switcher, grab);
-	enum wl_keyboard_key_state state = state_w;
+	enum wl_keyboard_key_state state = key_event->key_state;
+	uint32_t key = key_event->key;
 
 	if (key == KEY_TAB && state == WL_KEYBOARD_KEY_STATE_PRESSED)
 		switcher_next(switcher);

frontend/text-backend.c
@@ -621,13 +621,15 @@ unbind_keyboard(struct wl_resource *resource)
 
 static void
 input_method_context_grab_key(struct weston_keyboard_grab *grab,
-			      const struct timespec *time, uint32_t key,
-			      uint32_t state_w)
+			      const struct weston_key_event *key_event)
 {
 	struct weston_keyboard *keyboard = grab->keyboard;
 	struct wl_display *display;
 	uint32_t serial;
 	uint32_t msecs;
+	uint32_t key = key_event->key;
+	struct timespec time = key_event->base.ts;
+	uint32_t state_w = key_event->key_state;
 
 	if (!keyboard->input_method_resource)
 		return;
@@ -635,7 +637,7 @@ input_method_context_grab_key(struct weston_keyboard_grab *grab,
 	display = wl_client_get_display(
 		wl_resource_get_client(keyboard->input_method_resource));
 	serial = wl_display_next_serial(display);
-	msecs = timespec_to_msec(time);
+	msecs = timespec_to_msec(&time);
 	wl_keyboard_send_key(keyboard->input_method_resource,
 			     serial, msecs, key, state_w);
 }
@@ -712,10 +714,14 @@ input_method_context_key(struct wl_client *client,
 	struct weston_keyboard *keyboard = weston_seat_get_keyboard(seat);
 	struct weston_keyboard_grab *default_grab = &keyboard->default_grab;
 	struct timespec ts;
+	struct weston_key_event key_event;
 
 	timespec_from_msec(&ts, time);
 
-	default_grab->interface->key(default_grab, &ts, key, state_w);
+	weston_key_event_init(&key_event, &ts, seat, key,
+			      state_w, STATE_UPDATE_NONE);
+
+	default_grab->interface->key(default_grab, &key_event);
 }
 
 static void

include/libweston/libweston.h
@@ -188,6 +188,11 @@ struct weston_spring {
 	uint32_t clip;
 };
 
+enum weston_key_state_update {
+	STATE_UPDATE_AUTOMATIC,
+	STATE_UPDATE_NONE,
+};
+
 /* bit compatible with drm definitions. */
 enum dpms_enum {
 	WESTON_DPMS_ON,
@@ -615,6 +620,20 @@ struct weston_pointer_axis_event {
 	int32_t discrete;
 };
 
+/* base/common struct which all weston_xxx_event should "inherit" */
+struct weston_input_event {
+       struct timespec ts;
+       struct weston_seat *seat;
+};
+
+struct weston_key_event {
+	struct weston_input_event base;
+	uint32_t key;
+	enum wl_keyboard_key_state key_state;
+	enum weston_key_state_update key_update_state;
+};
+
+
 struct weston_pointer_grab;
 struct weston_pointer_grab_interface {
 	void (*focus)(struct weston_pointer_grab *grab);
@@ -640,7 +659,7 @@ struct weston_pointer_grab {
 struct weston_keyboard_grab;
 struct weston_keyboard_grab_interface {
 	void (*key)(struct weston_keyboard_grab *grab,
-		    const struct timespec *time, uint32_t key, uint32_t state);
+		    const struct weston_key_event *key_event);
 	void (*modifiers)(struct weston_keyboard_grab *grab, uint32_t serial,
 			  uint32_t mods_depressed, uint32_t mods_latched,
 			  uint32_t mods_locked, uint32_t group);
@@ -1003,8 +1022,14 @@ weston_keyboard_set_locks(struct weston_keyboard *keyboard,
 
 void
 weston_keyboard_send_key(struct weston_keyboard *keyboard,
-			 const struct timespec *time, uint32_t key,
-			 enum wl_keyboard_key_state state);
+			 const struct weston_key_event *key_event);
+
+void
+weston_key_event_init(struct weston_key_event *event, struct timespec *ts,
+		      struct weston_seat *seat, uint32_t key,
+		      enum wl_keyboard_key_state key_state,
+		      enum weston_key_state_update key_update_state);
+
 void
 weston_keyboard_send_modifiers(struct weston_keyboard *keyboard,
 			       uint32_t serial, uint32_t mods_depressed,
@@ -2123,12 +2148,6 @@ struct content_protection {
 	struct weston_log_scope *debug;
 	struct wl_list protected_list;
 	struct wl_event_source *surface_protection_update;
-};
-
-
-enum weston_key_state_update {
-	STATE_UPDATE_AUTOMATIC,
-	STATE_UPDATE_NONE,
 };
 
 enum weston_activate_flag {

libweston/backend-rdp/rdp.c
@@ -1573,6 +1573,7 @@ xf_input_keyboard_event(rdpInput *input, UINT16 flags, XF_KEV_CODE_TYPE code)
 	bool send_release_key = false;
 	int notify = 0;
 	struct timespec time;
+	struct weston_key_event key_event;
 
         rdp_debug_verbose(peerContext->rdpBackend, "RDP backend: %s flags:0x%x, code:0x%x\n",
 			  __func__, flags, code);
@@ -1638,14 +1639,17 @@ xf_input_keyboard_event(rdpInput *input, UINT16 flags, XF_KEV_CODE_TYPE code)
 		/*weston_log("code=%x ext=%d vk_code=%x scan_code=%x\n", code, (flags & KBD_FLAGS_EXTENDED) ? 1 : 0,
 				vk_code, scan_code);*/
 		weston_compositor_get_time(&time);
-		notify_key(peerContext->item.seat, &time,
-					scan_code - 8, keyState, STATE_UPDATE_AUTOMATIC);
+
+		weston_key_event_init(&key_event, &time, peerContext->item.seat,
+				      scan_code - 8, keyState,
+				      STATE_UPDATE_AUTOMATIC);
+		notify_key(&key_event);
 
 		if (send_release_key) {
-			notify_key(peerContext->item.seat, &time,
-				   scan_code - 8,
-				   WL_KEYBOARD_KEY_STATE_RELEASED,
-				   STATE_UPDATE_AUTOMATIC);
+			weston_key_event_init(&key_event, &time, peerContext->item.seat,
+					      scan_code - 8, WL_KEYBOARD_KEY_STATE_RELEASED,
+					      STATE_UPDATE_AUTOMATIC);
+			notify_key(&key_event);
 		}
 	}
 

libweston/backend-vnc/vnc.c
@@ -301,6 +301,7 @@ vnc_handle_key_event(struct nvnc_client *client, uint32_t keysym,
 	bool needs_shift = false;
 	enum weston_key_state_update state_update;
 	enum wl_keyboard_key_state state;
+	struct weston_key_event key_event;
 	struct timespec time;
 	int i;
 
@@ -337,19 +338,27 @@ vnc_handle_key_event(struct nvnc_client *client, uint32_t keysym,
 	}
 
 	/* emulate lshift press */
-	if (needs_shift)
-		notify_key(peer->seat, &time, KEY_LEFTSHIFT,
-			   WL_KEYBOARD_KEY_STATE_PRESSED,
-			   STATE_UPDATE_AUTOMATIC);
+	if (needs_shift) {
+		weston_key_event_init(&key_event, &time, peer->seat,
+				      KEY_LEFTSHIFT,
+				      WL_KEYBOARD_KEY_STATE_PRESSED,
+				      STATE_UPDATE_AUTOMATIC);
+		notify_key(&key_event);
+	}
 
 	/* send detected key code */
-	notify_key(peer->seat, &time, key, state, state_update);
+	weston_key_event_init(&key_event, &time, peer->seat,
+			      key, state, state_update);
+	notify_key(&key_event);
 
 	/* emulate lshift release */
-	if (needs_shift)
-		notify_key(peer->seat, &time, KEY_LEFTSHIFT,
-			   WL_KEYBOARD_KEY_STATE_RELEASED,
-			   STATE_UPDATE_AUTOMATIC);
+	if (needs_shift) {
+		weston_key_event_init(&key_event, &time, peer->seat,
+				      KEY_LEFTSHIFT,
+				      WL_KEYBOARD_KEY_STATE_RELEASED,
+				      STATE_UPDATE_AUTOMATIC);
+		notify_key(&key_event);
+	}
 }
 
 static void
@@ -359,6 +368,7 @@ vnc_handle_key_code_event(struct nvnc_client *client, uint32_t key,
 	struct vnc_peer *peer = nvnc_get_userdata(client);
 	enum wl_keyboard_key_state state;
 	struct timespec time;
+	struct weston_key_event key_event;
 
 	weston_compositor_get_time(&time);
 
@@ -367,7 +377,9 @@ vnc_handle_key_code_event(struct nvnc_client *client, uint32_t key,
 	else
 		state = WL_KEYBOARD_KEY_STATE_RELEASED;
 
-	notify_key(peer->seat, &time, key, state, STATE_UPDATE_AUTOMATIC);
+	weston_key_event_init(&key_event, &time, peer->seat,
+			      key, state, STATE_UPDATE_AUTOMATIC);
+	notify_key(&key_event);
 }
 
 static void

libweston/backend-wayland/wayland.c
@@ -2004,6 +2004,7 @@ input_handle_key(void *data, struct wl_keyboard *keyboard,
 {
 	struct wayland_input *input = data;
 	struct timespec ts;
+	struct weston_key_event key_event;
 
 	if (!input->keyboard_focus)
 		return;
@@ -2011,10 +2012,11 @@ input_handle_key(void *data, struct wl_keyboard *keyboard,
 	timespec_from_msec(&ts, time);
 
 	input->key_serial = serial;
-	notify_key(&input->base, &ts, key,
-		   state ? WL_KEYBOARD_KEY_STATE_PRESSED :
-			   WL_KEYBOARD_KEY_STATE_RELEASED,
-		   input->keyboard_state_update);
+
+	weston_key_event_init(&key_event, &ts, &input->base,
+			      key, state ? WL_KEYBOARD_KEY_STATE_PRESSED :
+			      WL_KEYBOARD_KEY_STATE_RELEASED, input->keyboard_state_update);
+	notify_key(&key_event);
 }
 
 static void

libweston/backend.h
@@ -242,9 +242,8 @@ notify_button(struct weston_seat *seat, const struct timespec *time,
 	      int32_t button, enum wl_pointer_button_state state);
 
 void
-notify_key(struct weston_seat *seat, const struct timespec *time, uint32_t key,
-	   enum wl_keyboard_key_state state,
-	   enum weston_key_state_update update_state);
+notify_key(const struct weston_key_event *key_event);
+
 void
 notify_keyboard_focus_in(struct weston_seat *seat, struct wl_array *keys,
 			 enum weston_key_state_update update_state);

commit 0123bee2730d380f244decebcb6cbde5b2de5ae9

x11-backend: fix key-press typo
Probably copy-pasta. Noticed by gcc 15

../../git/weston/libweston/backend-x11/x11.c: In function ‘x11_backend_handle_event’:
../../git/weston/libweston/backend-x11/x11.c:1687:58: error: ‘key_release’ may be used uninitialized [-Werror=maybe-uninitialized]
 1687 |                                               key_release->detail - 8,

Fixes: 99527e6b927b261f21d2a3428f6ac65c69e48713
Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

libweston/backend-x11/x11.c
@@ -1684,7 +1684,7 @@ x11_backend_handle_event(int fd, uint32_t mask, void *data)
 			struct weston_key_event key_event;
 
 			weston_key_event_init(&key_event, &time, &b->core_seat,
-					      key_release->detail - 8,
+					      key_press->detail - 8,
 					      WL_KEYBOARD_KEY_STATE_PRESSED,
 					      b->has_xkb ? STATE_UPDATE_NONE : STATE_UPDATE_AUTOMATIC);
 			notify_key(&key_event);