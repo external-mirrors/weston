8bb82f341e8cd7169c11fd7950f9b59239ac6037
Erico Nunes <nunes.erico@gmail.com>
2026-05-26 12:29:57 +0200

    vulkan-renderer: Fix renderpass subpass dependency
    The renderpass implicit subpass dependency effectively waits for
    nothing, which can be a source of hazard for swapchain outputs.
    As reported by synchronization validation, the layout transition
    implicit in the renderpass must wait for COLOR_ATTACHMENT_OUTPUT of
    the preceding vkAcquireNextImageKHR.
    
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

--- libweston/renderer-vulkan/vulkan-renderer.c
+++ libweston/renderer-vulkan/vulkan-renderer.c
@@ -3616,12 +3616,23 @@ create_renderpass(struct weston_output *output, VkFormat format, VkImageLayout a
 		.colorAttachmentCount = 1,
 		.pColorAttachments = &attachment_reference,
 	};
+	const VkSubpassDependency subpass_dependency = {
+		.srcSubpass = VK_SUBPASS_EXTERNAL,
+		.dstSubpass = 0,
+		.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+		.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+		.srcAccessMask = 0,
+		.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT |
+				 VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+	};
 	const VkRenderPassCreateInfo renderpass_create_info = {
 		.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
 		.attachmentCount = 1,
 		.pAttachments = &attachment_description,
 		.subpassCount = 1,
 		.pSubpasses = &subpass_description,
+		.dependencyCount = 1,
+		.pDependencies = &subpass_dependency,
 	};
 
 	result = vkCreateRenderPass(vr->dev, &renderpass_create_info, NULL, &vo->renderpass);
