external-mirrors/weston

commit 82d62974164e6a3fc64a69d876e6725763023ea3

gl-shaders: Remove asserts relying on shader compiler behavior
yuv_coefficients and yuv_offsets should get optimized away by shader
compilers as the related code paths can never be reached. This seems to
work well on Mesa but not necessarily with other drivers.

While on it, assert that the uniforms *are* present, unless the
yuv-to-rgb conversion is handled by the driver.

Signed-off-by: Robert Mader <robert.mader@collabora.com>
(cherry picked from commit 5c49563ef49a502a6b8fae44f8778e109d32c2d9)

libweston/renderer-gl/gl-shaders.c
@@ -762,8 +762,18 @@ gl_shader_load_config_representation(struct weston_compositor *compositor,
 
 	if (sconf->yuv_coefficients == WESTON_COLOR_MATRIX_COEF_UNSET ||
 	    sconf->yuv_coefficients == WESTON_COLOR_MATRIX_COEF_IDENTITY) {
-		assert(shader->yuv_coefficients_uniform == -1);
-		assert(shader->yuv_offsets_uniform == -1);
+		/*
+		 * In this case the yuv_coefficients and yuv_offsets uniforms
+		 * should never be used and thus get optimized away by the
+		 * shader compiler. Unfortunately on some drivers this is not
+		 * the case, so we can't assert on it.
+		 */
+		return;
+	}
+
+	if (shader->yuv_coefficients_uniform == -1 ||
+	    shader->yuv_offsets_uniform == -1) {
+		assert(shader->key.variant == SHADER_VARIANT_EXTERNAL);
 		return;
 	}
 

commit b3ba4e9e3d963da793d7494206c9cb7fa03f8480

gl-shaders: Allow SHADER_VARIANT_RGBA for YUV->RGB conversion
This assert is hit on drivers supporting YUV formats as GL_TEXTURE_2D
texture target instead of GL_TEXTURE_EXTERNAL_OES. While uncommon, it is
completely valid and we explicitly support it in related code paths.

Fixes: 82d629741 (gl-shaders: Remove asserts relying on shader compiler behavior)
Signed-off-by: Robert Mader <robert.mader@collabora.com>

libweston/renderer-gl/gl-shaders.c
@@ -824,7 +824,8 @@ gl_shader_load_config_representation(struct weston_compositor *compositor,
 
 	if (shader->yuv_coefficients_uniform == -1 ||
 	    shader->yuv_offsets_uniform == -1) {
-		assert(shader->key.variant == SHADER_VARIANT_EXTERNAL);
+		assert(shader->key.variant == SHADER_VARIANT_EXTERNAL ||
+		       shader->key.variant == SHADER_VARIANT_RGBA);
 		return;
 	}
 