cfc1a6726db820f6f7ba6064ddd0762d740da041
Derek Foreman <derek.foreman@collabora.com>
2026-06-29 10:36:08 -0500

    trace: store Global track ids
    When you pass a custom track id to perfetto, it hashes it with the parent
    track id to try to make a globally unique track uuid. Next time you need
    the same track, you can do Track(custom_id), or Track::Global(uuid).
    
    When we make a track heirachy in an upcoming commit, we need the uuid to
    set parenting information. Storing just the uuid lets us look up tracks
    or set parent tracks from the same stored uint64_t without extra steps.
    
    Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

--- libweston/perfetto/u_perfetto.cc
+++ libweston/perfetto/u_perfetto.cc
@@ -94,7 +94,7 @@ util_perfetto_trace_full_begin(const char *fname, uint64_t track_id, uint64_t id
 
 	TRACE_EVENT_BEGIN(UTIL_PERFETTO_CATEGORY_DEFAULT_STR,
 			  nullptr,
-			  perfetto::Track(track_id),
+			  track_id ? perfetto::Track::Global(track_id) : perfetto::Track(0),
 			  perfetto::TraceTimestamp{clockid_to_perfetto_clock(clock), timestamp},
 			  [&](perfetto::EventContext ctx) {
 				ctx.event()->set_name(fname);
@@ -112,14 +112,14 @@ util_perfetto_new_track(const char *name)
 
 	desc.set_name(name);
 	perfetto::TrackEvent::SetTrackDescriptor(track, desc);
-	return track_id;
+	return track.uuid;
 }
 
 void
 util_perfetto_trace_full_end(uint64_t track_id, clockid_t clock, uint64_t timestamp)
 {
 	TRACE_EVENT_END(UTIL_PERFETTO_CATEGORY_DEFAULT_STR,
-			perfetto::Track(track_id),
+			track_id ? perfetto::Track::Global(track_id) : perfetto::Track(0),
 			perfetto::TraceTimestamp{clockid_to_perfetto_clock(clock), timestamp});
 
 	util_perfetto_update_tracing_state();
@@ -137,7 +137,7 @@ util_perfetto_trace_instant_timestamp(const char *name, uint64_t track_id, uint6
 {
 	TRACE_EVENT_INSTANT(UTIL_PERFETTO_CATEGORY_DEFAULT_STR,
 			    nullptr,
-			    perfetto::Track(track_id),
+			    track_id ? perfetto::Track::Global(track_id) : perfetto::Track(0),
 			    perfetto::TraceTimestamp{clockid_to_perfetto_clock(clock), ts},
 			    [&](perfetto::EventContext ctx) {
 				ctx.event()->set_name(name);
