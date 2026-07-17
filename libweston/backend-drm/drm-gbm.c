external-mirrors/weston

commit 853aa1c5641a06ed7f75d2833f3e3e5da9f23a04

backend-drm: destroy_sprites() after outputs
I want to consolidate 'struct drm_device' destruction into a single
function in the next patch, and that function needs to be usable for
both the primary device drm_backend::drm, and the secondary KMS devices
drm_backend::kms_list.

Sprites, a.k.a drm_planes, are objects fundamentally owned by their
drm_device. To encapsulate drm_device destruction into a single function
(that assumes that all outputs on that device are already destroyed),
one cannot call destroy_sprites() from drm_shutdown() anymore - it
logically belongs in drm_device_destroy().

The problem is that that will reverse the order of destroying drm_planes
vs. output (GBM) surfaces. drm_plane_state holds a reference to drm_fb
which points to a GBM BO that was received from a GBM surface. When the
GBM surface is destroyed, all its GBM BOs are destroyed as well, which
will cause all drm_fb pointing to them to be destroyed as well
regardless of drm_fb reference count. Therefore, if any object was
thought to hold a reference to a drm_fb, it now holds a dangling
pointer.

Why is drm_output_deinit() not clearing the drm_plane state it used,
releasing all the drm_fb references? And do that before destroying the
GBM surface?

Because drm_output_deinit_planes() explicitly skips clearing the
drm_plane state during compositor shutdown, because during shutdown the
drm_planes have already been destroyed.

If we change both, we get a much simpler tear-down logic:

- drm_output_deinit_planes() will always clear drm_plane states, no
  special handling for shutdown (which is an elaborate way of saying
  "before outputs are destroyed").

- drm_output_deinit() calls drm_output_deinit_planes() before it
  destroys the GBM surface, which ensures no dangling drm_fb pointers
  are left.

- destroy_sprites() call is moved *after* destruction of outputs where
  it logically belongs.

This also means that the per-renderer fini functions do not need to

libweston/backend-drm/drm-gbm.c
@@ -753,12 +753,8 @@ drm_output_fini_egl(struct drm_output *output)
 	const struct weston_renderer *renderer = b->compositor->renderer;
 
 	/* Destroying the GBM surface will destroy all our GBM buffers,
-	 * regardless of refcount. Ensure we destroy them here. */
-	if (!b->compositor->shutting_down &&
-	    output->scanout_plane->state_cur->fb &&
-	    output->scanout_plane->state_cur->fb->type == BUFFER_GBM_SURFACE) {
-		drm_plane_reset_state(output->scanout_plane);
-	}
+	 * regardless of refcount. */
+	weston_assert_ptr_null(b->compositor, output->scanout_plane);
 
 	renderer->gl->output_destroy(&output->base);
 	gbm_surface_destroy(output->gbm_surface);
@@ -772,11 +768,7 @@ drm_output_fini_vulkan(struct drm_output *output)
 	struct drm_backend *b = output->backend;
 	const struct weston_renderer *renderer = b->compositor->renderer;
 
-	if (!b->compositor->shutting_down &&
-	    output->scanout_plane->state_cur->fb &&
-	    output->scanout_plane->state_cur->fb->type == BUFFER_DMABUF_BACKEND) {
-		drm_plane_reset_state(output->scanout_plane);
-	}
+	weston_assert_ptr_null(b->compositor, output->scanout_plane);
 
 	for (unsigned int i = 0; i < ARRAY_LENGTH(output->renderbuffer); i++)
 		renderer->destroy_renderbuffer(output->renderbuffer[i]);

libweston/backend-drm/drm.c
@@ -1960,12 +1960,8 @@ drm_output_fini_pixman(struct drm_output *output)
 	unsigned int i;
 
 	/* Destroying the Pixman surface will destroy all our buffers,
-	 * regardless of refcount. Ensure we destroy them here. */
-	if (!b->compositor->shutting_down &&
-	    output->scanout_plane->state_cur->fb &&
-	    output->scanout_plane->state_cur->fb->type == BUFFER_PIXMAN_DUMB) {
-		drm_plane_reset_state(output->scanout_plane);
-	}
+	 * regardless of refcount. */
+	weston_assert_ptr_null(b->compositor, output->scanout_plane);
 
 	for (i = 0; i < ARRAY_LENGTH(output->dumb); i++) {
 		renderer->destroy_renderbuffer(output->renderbuffer[i]);
@@ -2608,32 +2604,27 @@ drm_output_init_planes(struct drm_output *output)
 static void
 drm_output_deinit_planes(struct drm_output *output)
 {
-	struct drm_backend *b = output->backend;
 	struct drm_device *device = output->device;
 
-	/* If the compositor is already shutting down, the planes have already
-	 * been destroyed. */
-	if (!b->compositor->shutting_down) {
-		wl_list_remove(&output->scanout_plane->base.link);
-		wl_list_init(&output->scanout_plane->base.link);
+	wl_list_remove(&output->scanout_plane->base.link);
+	wl_list_init(&output->scanout_plane->base.link);
 
-		if (output->cursor_plane) {
-			wl_list_remove(&output->cursor_plane->base.link);
-			wl_list_init(&output->cursor_plane->base.link);
-			/* Turn off hardware cursor */
-			drmModeSetCursor(device->kms_device->fd, output->crtc->crtc_id, 0, 0, 0);
-		}
-
-		/* With universal planes, the planes are allocated at startup,
-		 * freed at shutdown, and live on the plane list in between.
-		 * We want the planes to  continue to exist and be freed up
-		 * for other outputs.
-		 */
-		if (output->cursor_plane)
-			drm_plane_reset_state(output->cursor_plane);
-		if (output->scanout_plane)
-			drm_plane_reset_state(output->scanout_plane);
+	if (output->cursor_plane) {
+		wl_list_remove(&output->cursor_plane->base.link);
+		wl_list_init(&output->cursor_plane->base.link);
+		/* Turn off hardware cursor */
+		drmModeSetCursor(device->kms_device->fd, output->crtc->crtc_id, 0, 0, 0);
 	}
+
+	/* With universal planes, the planes are allocated at startup,
+	 * freed at shutdown, and live on the plane list in between.
+	 * We want the planes to  continue to exist and be freed up
+	 * for other outputs.
+	 */
+	if (output->cursor_plane)
+		drm_plane_reset_state(output->cursor_plane);
+	if (output->scanout_plane)
+		drm_plane_reset_state(output->scanout_plane);
 
 	output->cursor_plane = NULL;
 	output->scanout_plane = NULL;
@@ -2868,6 +2859,13 @@ drm_output_deinit(struct weston_output *base)
 		drm_pending_state_apply_sync(pending);
 	}
 
+	/*
+	 * Remove all potential drm_fb references to GBM BOs, so that the
+	 * renderer tear-down can destroy the originating GBM/Vulkan
+	 * surface without leaving dangling drm_fb pointers.
+	 */
+	drm_output_deinit_planes(output);
+
 	if (b->compositor->renderer->type == WESTON_RENDERER_PIXMAN)
 		drm_output_fini_pixman(output);
 	else if (b->compositor->renderer->type == WESTON_RENDERER_VULKAN)
@@ -2875,7 +2873,6 @@ drm_output_deinit(struct weston_output *base)
 	else
 		drm_output_fini_egl(output);
 
-	drm_output_deinit_planes(output);
 	drm_output_detach_crtc(output);
 
 	output->blend_to_output_xform = NULL;
@@ -4034,8 +4031,6 @@ drm_shutdown(struct weston_backend *backend)
 		}
 	}
 
-	destroy_sprites(b->drm);
-
 	weston_log_scope_destroy(b->debug);
 	b->debug = NULL;
 }
@@ -4083,6 +4078,8 @@ drm_destroy(struct weston_backend *backend)
 	if (b->gbm)
 		gbm_device_destroy(b->gbm);
 #endif
+
+	destroy_sprites(device);
 
 	drm_kms_device_destroy(device->kms_device);
 

commit b8059236277f53fdf5184417d0c2d598d1cdb5fd

drm: fix mode changes at runtime
drm_output_fini_egl() / drm_output_fini_vulkan() assume that all planes are
removed first. This is necessary because all originating GBM/Vulkan surfaces are
destroyed during fini.

Partitally deinitialize the planes to ensure that no surfaces owned by the
renderer are still used.

Fixes: commit 853aa1c5641a06ed7f75d2833f3e3e5da9f23a04 ("backend-drm:
destroy_sprites() after outputs")

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

libweston/backend-drm/drm-gbm.c
@@ -757,7 +757,12 @@ drm_output_fini_egl(struct drm_output *output)
 
 	/* Destroying the GBM surface will destroy all our GBM buffers,
 	 * regardless of refcount. */
-	weston_assert_ptr_null(b->compositor, output->scanout_handle);
+	if (b->compositor->shutting_down) {
+		weston_assert_ptr_null(b->compositor, output->scanout_handle);
+	} else {
+		weston_assert_ptr_null(b->compositor,
+				       output->scanout_handle->plane->state_cur->fb);
+	}
 
 	renderer->gl->output_destroy(&output->base);
 	gbm_surface_destroy(output->gbm_surface);
@@ -771,7 +776,12 @@ drm_output_fini_vulkan(struct drm_output *output)
 	struct drm_backend *b = output->backend;
 	const struct weston_renderer *renderer = b->compositor->renderer;
 
-	weston_assert_ptr_null(b->compositor, output->scanout_handle);
+	if (b->compositor->shutting_down) {
+		weston_assert_ptr_null(b->compositor, output->scanout_handle);
+	} else {
+		weston_assert_ptr_null(b->compositor,
+				       output->scanout_handle->plane->state_cur->fb);
+	}
 
 	for (unsigned int i = 0; i < ARRAY_LENGTH(output->renderbuffer); i++)
 		renderer->destroy_renderbuffer(output->renderbuffer[i]);

libweston/backend-drm/drm.c
@@ -1360,6 +1360,9 @@ drm_output_switch_mode(struct weston_output *output_base, struct weston_mode *mo
 	return drm_output_apply_mode(output);
 }
 
+static void
+drm_output_reset_planes(struct drm_output *output);
+
 static int
 drm_output_apply_mode(struct drm_output *output)
 {
@@ -1380,6 +1383,13 @@ drm_output_apply_mode(struct drm_output *output)
 
 	if (!weston_renderer_resize_output(&output->base, &fb_size, NULL))
 		return -1;
+
+	/*
+	 * Remove all potential drm_fb references to GBM BOs, so that the
+	 * renderer tear-down can destroy the originating GBM/Vulkan
+	 * surface without leaving dangling drm_fb pointers.
+	 */
+	drm_output_reset_planes(output);
 
 	if (b->compositor->renderer->type == WESTON_RENDERER_GL) {
 		drm_output_fini_egl(output);
@@ -2717,19 +2727,30 @@ drm_output_init_planes(struct drm_output *output)
 	return 0;
 }
 
+static void
+drm_output_reset_planes(struct drm_output *output)
+{
+	struct drm_device *device = output->device;
+
+	if (output->cursor_handle) {
+		/* Turn off hardware cursor */
+		drmModeSetCursor(device->kms_device->fd, output->crtc->crtc_id, 0, 0, 0);
+		drm_plane_reset_state(output->cursor_handle->plane);
+	}
+
+	if (output->scanout_handle)
+		drm_plane_reset_state(output->scanout_handle->plane);
+}
+
 /** The opposite of drm_output_init_planes(). First of all it removes the planes
  * from the plane stacking list. After all it sets the planes of the output as NULL.
  */
 static void
 drm_output_deinit_planes(struct drm_output *output)
 {
-	struct drm_device *device = output->device;
 	struct drm_plane_handle *handle, *next_handle;
 
-	if (output->cursor_handle) {
-		/* Turn off hardware cursor */
-		drmModeSetCursor(device->kms_device->fd, output->crtc->crtc_id, 0, 0, 0);
-	}
+	drm_output_reset_planes(output);
 
 	/* With universal planes, the planes are allocated at startup,
 	 * freed at shutdown, and live on the plane list in between.
@@ -2737,13 +2758,11 @@ drm_output_deinit_planes(struct drm_output *output)
 	 * for other outputs.
 	 */
 	if (output->cursor_handle) {
-		drm_plane_reset_state(output->cursor_handle->plane);
 		drm_plane_destroy_handle(output->cursor_handle);
 		output->cursor_handle = NULL;
 	}
 
 	if (output->scanout_handle) {
-		drm_plane_reset_state(output->scanout_handle->plane);
 		drm_plane_destroy_handle(output->scanout_handle);
 		output->scanout_handle = NULL;
 	}