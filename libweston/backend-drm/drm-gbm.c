CodeLinaro-mirror/la_wayland_weston

commit de75997abc08284385c088752052c7e963e19b6f

drm-backend: Use explicit synchronisation for Vulkan
We can't rely on Vulkan implementations placing an implicit fence on our
dmabuf, so let's do it ourselves.

Signed-off-by: Daniel Stone <daniels@collabora.com>

libweston/backend-drm/drm-gbm.c
@@ -816,13 +816,15 @@ struct drm_fb *
 drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *damage)
 {
 	struct drm_output *output = state->output;
+	struct drm_plane_state *pstate =
+		drm_output_state_get_plane(state, output->scanout_handle->plane);
+	struct weston_renderer *renderer = output->base.compositor->renderer;
 	struct drm_device *device = output->device;
 	struct linux_dmabuf_memory *dmabuf;
 	struct drm_fb *ret;
 
-	output->base.compositor->renderer->repaint_output(&output->base,
-							  damage,
-							  output->renderbuffer[output->current_image]);
+	renderer->repaint_output(&output->base, damage,
+				 output->renderbuffer[output->current_image]);
 
 	dmabuf = output->linux_dmabuf_memory[output->current_image];
 	if (!dmabuf) {
@@ -837,6 +839,13 @@ drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *dama
 						false, true, NULL);
 	if (!ret) {
 		weston_log("failed to get drm_fb for dmabuf\n");
+		return NULL;
+	}
+
+	pstate->in_fence_fd = renderer->vulkan->create_fence_fd(&output->base);
+	if (pstate->in_fence_fd < 1) {
+		weston_log("failed to get fence fd from rendering\n");
+		drm_fb_unref(ret);
 		return NULL;
 	}
 

external-mirrors/weston

commit 6161d79e0c3f22834d8c1c2dcfd584261949cdca

drm-backend: let plane_state own in_fence_fd
Make drm_plane_state own in_fence_fd. Closing in_fence_fd in
drm_plane_state_free() fixes a leak of the already dup()ed
render_fence_fd from vulkan_output_state in drm_output_render_vulkan().
Also dup() the acquire_fence_fd from weston_surface in
drm_output_try_paint_node_on_plane() and
drm_output_propose_state_try_reuse().

Fixes: de75997abc08 ("drm-backend: Use explicit synchronisation for Vulkan")
Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

libweston/backend-drm/drm-gbm.c
@@ -45,6 +45,7 @@
 #include "shared/weston-egl-ext.h"
 #include "linux-dmabuf.h"
 #include "linux-explicit-synchronization.h"
+#include "shared/fd-util.h"
 #include "shared/xalloc.h"
 #include "weston-trace.h"
 
@@ -821,6 +822,7 @@ drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *dama
 	struct drm_device *device = output->device;
 	struct linux_dmabuf_memory *dmabuf;
 	struct drm_fb *ret;
+	int fence_fd;
 
 	renderer->repaint_output(&output->base, damage,
 				 output->renderbuffer[output->current_image]);
@@ -840,12 +842,13 @@ drm_output_render_vulkan(struct drm_output_state *state, pixman_region32_t *dama
 		return NULL;
 	}
 
-	pstate->in_fence_fd = renderer->vulkan->create_fence_fd(&output->base);
-	if (pstate->in_fence_fd < 1) {
+	fence_fd = renderer->vulkan->create_fence_fd(&output->base);
+	if (fence_fd < 0) {
 		weston_log("failed to get fence fd from rendering\n");
 		drm_fb_unref(ret);
 		return NULL;
 	}
+	fd_update(&pstate->in_fence_fd, fence_fd);
 
 	output->current_image = (output->current_image + 1) % ARRAY_LENGTH(output->renderbuffer);
 

libweston/backend-drm/drm-internal.h
@@ -494,7 +494,6 @@ struct drm_plane_state {
 
 	bool complete;
 
-	/* We don't own the fd, so we shouldn't close it */
 	int in_fence_fd;
 
 	uint32_t damage_blob_id; /* damage to kernel */

libweston/backend-drm/state-helpers.c
@@ -34,6 +34,7 @@
 
 #include "colorops.h"
 #include "drm-internal.h"
+#include "shared/fd-util.h"
 #include "shared/weston-assert.h"
 #include "shared/weston-drm-fourcc.h"
 #include "shared/xalloc.h"
@@ -95,7 +96,7 @@ drm_plane_state_free(struct drm_plane_state *state, bool force)
 	wl_list_remove(&state->link);
 	wl_list_init(&state->link);
 	state->output_state = NULL;
-	state->in_fence_fd = -1;
+	fd_update(&state->in_fence_fd, -1);
 	state->zpos = DRM_PLANE_ZPOS_INVALID_PLANE;
 	state->alpha = DRM_PLANE_ALPHA_OPAQUE;
 	drm_color_pipeline_state_unref(state->pipeline_state);

libweston/backend-drm/state-propose.c
@@ -45,6 +45,7 @@
 #include "presentation-time-server-protocol.h"
 #include "linux-dmabuf-unstable-v1-server-protocol.h"
 #include "weston-trace.h"
+#include "shared/fd-util.h"
 #include "shared/string-helpers.h"
 #include "shared/weston-assert.h"
 
@@ -181,6 +182,7 @@ drm_output_try_paint_node_on_plane(struct drm_plane_handle *handle,
 	struct drm_plane *plane = handle->plane;
 	struct drm_plane_state *state = NULL;
 	enum blend_mode_error err_blend_mode;
+	int fence_fd = -1;
 
 	assert(!device->disable_client_buffer_scanout);
 	assert(output == handle->output);
@@ -212,7 +214,9 @@ drm_output_try_paint_node_on_plane(struct drm_plane_handle *handle,
 	 * here to live within the state. */
 	state->paint_node = pnode;
 	state->fb = drm_fb_ref(fb);
-	state->in_fence_fd = surface->acquire_fence_fd;
+	if (surface->acquire_fence_fd >= 0)
+		fence_fd = dup(surface->acquire_fence_fd);
+	fd_update(&state->in_fence_fd, fence_fd);
 
 	drm_color_pipeline_state_unref(state->pipeline_state);
 	state->pipeline_state = NULL;
@@ -1226,6 +1230,7 @@ drm_output_propose_state_try_reuse(struct weston_output *output_base,
 		struct drm_plane_state *pstate;
 		struct drm_plane *plane;
 		struct drm_fb *fb;
+		int fence_fd = -1;
 
 		/* we don't care about renderer views */
 		if (pnode->plane == &output_base->primary_plane) {
@@ -1278,7 +1283,9 @@ drm_output_propose_state_try_reuse(struct weston_output *output_base,
 		drm_fb_unref(pstate->fb);
 		pstate->fb = fb;
 
-		pstate->in_fence_fd = pnode->surface->acquire_fence_fd;
+		if (pnode->surface->acquire_fence_fd >= 0)
+			fence_fd = dup(pnode->surface->acquire_fence_fd);
+		fd_update(&pstate->in_fence_fd, fence_fd);
 		drm_debug(b, "\t\t[reuse] successfully stole away pnode %s to reused plane\n", pnode->internal_name);
 		/* XXX: When we set non-default color states in DRM, make sure they match */
 		weston_buffer_reference(&pstate->fb_ref.buffer,